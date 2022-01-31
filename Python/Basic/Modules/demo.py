b54f4664a89a728d19c278ba89e38c281232f28b
Shoikot Alam <96073081+shykat1@users.noreply.github.com>
2022-01-31 16:15:14 +0600

    Create demo.py

---
+++ Python/Basic/Modules/demo.py
@@ -0,0 +1,10 @@
+
+#> this will import every thing
+from Calc import *
+#from Calc import  add #> only to import any particular thing
+
+a = 10
+b = 10
+
+c= add(a,b) # this is the method we created in the calc.py
+print(c)
