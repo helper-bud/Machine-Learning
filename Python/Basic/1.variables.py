5cf1d9eae5942745ea9464fdcc5e13a48d8de5f3
Shoikot Alam <96073081+shykat1@users.noreply.github.com>
2022-02-01 08:17:26 +0600

    Create 1.variables.py

---
+++ Python/Basic/1.variables.py
@@ -0,0 +1,13 @@
+a = 123 #integer
+b = 1.23 #float
+c = 1+ 2j #complex
+d = "hello world"
+e = True
+f = False
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+type(a) # to see the type of the variable
