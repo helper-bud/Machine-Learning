1cf860da4f4934900598ce793ea3f1da2a514474
Shoikot Alam <96073081+shykat1@users.noreply.github.com>
2022-02-01 08:18:40 +0600

    Create 3. print.py

---
+++ Python/Basic/3. print.py
@@ -0,0 +1,13 @@
+a = 20
+b = 24
+c = 60
+
+resutl   = a *b * c
+
+print("20 days",resutl," in min:")
+
+print("20 days",str(resutl)," in min:")
+
+print(f"20 days {resutl} in min:")
+
+print(f"20 days { a * b * c} in min \n") 
