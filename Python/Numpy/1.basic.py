e7e9855e486e5e469da55676789b140ed22dd38e
Shoikot Alam <96073081+shykat1@users.noreply.github.com>
2022-02-04 02:31:30 +0600

    Create 1.basic.py

---
+++ Python/Numpy/1.basic.py
@@ -0,0 +1,7 @@
+#> basic
+a = np.array([1,2,3])
+print(a) # prints the array
+print(a.shape) # show the dimension and number of elements.
+print(a.dtype) # prints the data type
+print(a.size) # prints the size of the array
+print(a.itemsize) # show the element size in bytes
